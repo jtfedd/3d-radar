d09c651b1e97ccd19c91e63c707d8c4f23d74377
Jacob Feddersen <jtfedd@gmail.com>
2023-04-28 17:59:15 -0500

    Add more data point tests

--- test/model/test_data_point.py
+++ test/model/test_data_point.py
@@ -1,5 +1,7 @@
 from src.model.data_point import DataPoint
 
+import random
+
 import unittest
 
 class TestDataPoint(unittest.TestCase):
@@ -9,3 +11,21 @@ class TestDataPoint(unittest.TestCase):
         output = DataPoint.deserialize(bytes)
 
         self.assertEqual(input, output)
+
+    def test_serialize_floats(self):
+        input = DataPoint(1.1112, 2.32123, 42, 0.111222229)
+        bytes = input.serialize()
+        output = DataPoint.deserialize(bytes)
+
+        self.assertEqual(input, output)
+
+    def test_serialize_offset(self):
+        input = DataPoint(1, 2, 3, 10)
+        buffer = bytearray(random.randbytes(100))
+        offset = 21
+
+        writeOffset = input.writeBytes(buffer, offset)
+        output, readOffset = input.fromSerial(buffer, offset)
+
+        self.assertEqual(input, output)
+        self.assertEqual(writeOffset, readOffset)
