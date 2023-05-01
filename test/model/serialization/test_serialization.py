b665833f7bc8c16087bdc5530569c0e1c07421a2
Jacob Feddersen <jtfedd@gmail.com>
2023-05-01 07:57:39 -0500

    Consume new serialization method

--- test/model/serialization/test_serialization.py
+++ test/model/serialization/test_serialization.py
@@ -7,23 +7,26 @@ class TestSerialization(unittest.TestCase):
     def test_serialize_ray(self):
         input = newTestRay()
         bytes = serialization.serializeRay(input)
-        output, size = serialization.deserializeRay(bytes)
+        self.assertEqual(len(bytes), 116)
 
+        output, size = serialization.deserializeRay(bytes)
         assertRaysEqual(self, input, output)
-        self.assertEqual(size, 116)
+        self.assertEqual(size, len(bytes))
 
     def test_serialize_sweep(self):
         input = newTestSweep()
         bytes = serialization.serializeSweep(input)
-        output, size = serialization.deserializeSweep(bytes)
+        self.assertEqual(len(bytes), 1162)
 
+        output, size = serialization.deserializeSweep(bytes)
         assertSweepsEqual(self, input, output)
-        self.assertEqual(size, 1162)
+        self.assertEqual(size, len(bytes))
 
     def test_serialize_scan(self):
         input = newTestScan()
         bytes = serialization.serializeScan(input)
-        output, size = serialization.deserializeScan(bytes)
+        self.assertEqual(len(bytes), 11632)
 
+        output, size = serialization.deserializeScan(bytes)
         assertScansEqual(self, input, output)
-        self.assertEqual(size, 11632)
+        self.assertEqual(size, len(bytes))
