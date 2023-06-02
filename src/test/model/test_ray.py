5e2ed9c97530a623cf13f097bb3c7788c8e559f0
Jacob Feddersen <jtfedd@gmail.com>
2023-06-02 17:43:00 -0500

    Change coverage and see if it gets reported

--- src/test/model/test_ray.py
+++ src/test/model/test_ray.py
@@ -12,25 +12,25 @@ class TestDataPoint(unittest.TestCase):
         self.assertEqual(ray.range(1), 2.1)
         self.assertEqual(ray.range(10), 16.5)
 
-    def test_foreach(self):
-        ray = Ray(1, 1, 1, np.asarray([1, 2, np.nan, 3]))
+    # def test_foreach(self):
+    #     ray = Ray(1, 1, 1, np.asarray([1, 2, np.nan, 3]))
 
-        collectedPoints = []
-        ray.foreach(0.2, lambda p: collectedPoints.append(p))
+    #     collectedPoints = []
+    #     ray.foreach(0.2, lambda p: collectedPoints.append(p))
 
-        self.assertEqual(len(collectedPoints), 3)
+    #     self.assertEqual(len(collectedPoints), 3)
 
-        self.assertAlmostEqual(collectedPoints[0].x, 0.8246975884333746)
-        self.assertAlmostEqual(collectedPoints[0].y, 0.5295322319119196)
-        self.assertAlmostEqual(collectedPoints[0].z, 0.19866933079506122)
-        self.assertAlmostEqual(collectedPoints[0].value, 1)
+    #     self.assertAlmostEqual(collectedPoints[0].x, 0.8246975884333746)
+    #     self.assertAlmostEqual(collectedPoints[0].y, 0.5295322319119196)
+    #     self.assertAlmostEqual(collectedPoints[0].z, 0.19866933079506122)
+    #     self.assertAlmostEqual(collectedPoints[0].value, 1)
 
-        self.assertAlmostEqual(collectedPoints[1].x, 1.6493951768667492)
-        self.assertAlmostEqual(collectedPoints[1].y, 1.0590644638238391)
-        self.assertAlmostEqual(collectedPoints[1].z, 0.39733866159012243)
-        self.assertAlmostEqual(collectedPoints[1].value, 2)
+    #     self.assertAlmostEqual(collectedPoints[1].x, 1.6493951768667492)
+    #     self.assertAlmostEqual(collectedPoints[1].y, 1.0590644638238391)
+    #     self.assertAlmostEqual(collectedPoints[1].z, 0.39733866159012243)
+    #     self.assertAlmostEqual(collectedPoints[1].value, 2)
 
-        self.assertAlmostEqual(collectedPoints[2].x, 3.2987903537334984)
-        self.assertAlmostEqual(collectedPoints[2].y, 2.1181289276476782)
-        self.assertAlmostEqual(collectedPoints[2].z, 0.7946773231802449)
-        self.assertAlmostEqual(collectedPoints[2].value, 3)
+    #     self.assertAlmostEqual(collectedPoints[2].x, 3.2987903537334984)
+    #     self.assertAlmostEqual(collectedPoints[2].y, 2.1181289276476782)
+    #     self.assertAlmostEqual(collectedPoints[2].z, 0.7946773231802449)
+    #     self.assertAlmostEqual(collectedPoints[2].value, 3)
