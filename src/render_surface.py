d4a4f438c8ecd18942552329be3db14318f7848c
Jacob Feddersen <jtfedd@gmail.com>
2023-06-07 07:45:25 -0500

    Render multiple isosurfaces with different colors

--- src/render_surface.py
+++ src/render_surface.py
@@ -1,4 +1,4 @@
-from direct.showbase.ShowBase import ShowBase
+import direct.showbase.ShowBase
 
 from lib.camera.camera_control import CameraControl
 from lib.data_connector.data_connector import DataConnector
@@ -8,7 +8,7 @@ from lib.geometry import marching_cubes
 from lib.geometry import reshape
 from lib.geometry import triangles_to_geometry
 
-from panda3d.core import DirectionalLight, AmbientLight
+from panda3d.core import DirectionalLight, AmbientLight, TransparencyAttrib
 
 import numpy as np
 import datetime
@@ -34,11 +34,12 @@ def getData():
     return connector.load(record)
 
 
-class Viewer(ShowBase):
+class Viewer(direct.showbase.ShowBase.ShowBase):
     def __init__(self):
-        ShowBase.__init__(self)
+        direct.showbase.ShowBase.ShowBase.__init__(self)
         self.setBackgroundColor(0, 0, 0, 1)
         self.cameraControl = CameraControl(self)
+        self.accept("w", self.toggle_wireframe)
 
         # Make some light
         dlight = DirectionalLight("dlight")
@@ -48,7 +49,7 @@ class Viewer(ShowBase):
         self.render.setLight(dlnp)
 
         alight = AmbientLight("alight")
-        alight.setColor((0.2, 0.2, 0.2, 1))
+        alight.setColor((0.7, 0.7, 0.7, 1))
         alnp = self.render.attachNewNode(alight)
         self.render.setLight(alnp)
 
@@ -57,15 +58,36 @@ class Viewer(ShowBase):
         scan = getData()
 
         data = scan.reflectivity
-        data = np.isnan(data)
 
-        vertices, triangles = marching_cubes.getIsosurface(data, 0.5)
+        booleanData = np.isnan(data)
+        vertices, triangles = marching_cubes.getIsosurface(booleanData, 0.5)
         vertices = reshape.reshape(vertices, scan)
-
         geom = triangles_to_geometry.getGeometry(vertices, triangles, smooth=False)
         node = self.render.attachNewNode(geom)
+        node.setTransparency(TransparencyAttrib.MAlpha)
+        node.setColorScale(1, 1, 1, 0.1)
+
+        minValue = np.nanmin(data)
+        data = np.nan_to_num(data, nan=minValue)
+        data = np.negative(data)
+
+        self.addIso(data, scan, 5, 0, 0, 1, 0.2)
+        self.addIso(data, scan, -10, 0, 1, 0, 0.3)
+        self.addIso(data, scan, -35, 1, 1, 0, 0.4)
+        self.addIso(data, scan, -60, 1, 0, 0, 0.5)
 
         print("Done!")
+
+    def addIso(self, data, scan, level, r, g, b, a):
+        print("Iso", level)
+
+        vertices, triangles = marching_cubes.getIsosurface(data, level)
+        vertices = reshape.reshape(vertices, scan)
+        geom = triangles_to_geometry.getGeometry(vertices, triangles, smooth=False)
+
+        node = self.render.attachNewNode(geom)
+        node.setTransparency(TransparencyAttrib.MAlpha)
+        node.setColorScale(r, g, b, a)
 
 
 app = Viewer()
