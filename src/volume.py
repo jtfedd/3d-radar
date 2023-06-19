bbb49a76607432528d1c0d25ea114d4876f2efdd
Jacob Feddersen <jtfedd@gmail.com>
2023-06-19 11:56:51 -0500

    fix overriding the window event

--- src/volume.py
+++ src/volume.py
@@ -1,15 +1,15 @@
+from direct.showbase.DirectObject import DirectObject
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import Shader, WindowProperties
+from panda3d.core import GraphicsWindow, Shader
 
 
-class App(ShowBase):
-    def __init__(self) -> None:
-        ShowBase.__init__(self)
-        self.setBackgroundColor(0, 0, 0, 1)
+class App(DirectObject):
+    def __init__(self, showbase: ShowBase) -> None:
+        self.base = showbase
+
+        self.base.setBackgroundColor(0, 0, 0, 1)
         # self.wireframe On()
-        self.cam.setPos(0, -4, 0)
-
-        self.accept("window-event", self.handleWindowEvent)
+        self.base.cam.setPos(0, -40, 0)
 
         shader = Shader.load(
             Shader.SL_GLSL,
@@ -17,19 +17,25 @@ class App(ShowBase):
             fragment="shaders/volume-frag.glsl",
         )
 
-        plane = self.loader.loadModel("../assets/plane.glb")
+        plane = self.base.loader.loadModel("../assets/plane.glb")
         if plane:
             self.plane = plane
 
-        self.plane.reparentTo(self.render)
+        self.plane.reparentTo(self.base.render)
         self.plane.setP(90)
+        self.plane.setScale(100)
         self.plane.setShader(shader)
 
-        self.windowSize = self.getSize()
+        # For some reason this seems to be typed incorrectly; override the type
+        window: GraphicsWindow = self.base.win  # type: ignore
+        self.windowSize = (window.getXSize(), window.getYSize())
+
         self.plane.setShaderInput("resolution", self.windowSize)
 
-    def handleWindowEvent(self, _: WindowProperties) -> None:
-        newSize = self.getSize()
+        self.accept("window-event", self.handleWindowEvent)
+
+    def handleWindowEvent(self, win: GraphicsWindow) -> None:
+        newSize = (win.getXSize(), win.getYSize())
         if newSize[0] == self.windowSize[0] and newSize[1] == self.windowSize[1]:
             return
 
@@ -37,6 +43,6 @@ class App(ShowBase):
         self.plane.setShaderInput("resolution", self.windowSize)
 
 
-app = App()
-
-app.run()
+base = ShowBase()
+app = App(base)
+base.run()
