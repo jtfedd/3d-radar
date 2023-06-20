0c7579535c433c649d7e9a3932927c76d6bb87cb
Jacob Feddersen <jtfedd@gmail.com>
2023-06-19 21:03:53 -0500

    Render a sphere using the ray tracer

--- src/shaders/volume-frag.glsl
+++ src/shaders/volume-frag.glsl
@@ -3,12 +3,13 @@
 uniform mat4 p3d_ViewMatrixInverse;
 uniform mat4 p3d_ProjectionMatrixInverse;
 uniform vec2 resolution;
+uniform vec3 camera_position;
 
 out vec4 p3d_FragColor;
 
-void main() {
+vec3 gen_ray() {
     float x = (2.0f * gl_FragCoord.x) / resolution.x - 1.0f;
-    float y = 1.0f - (2.0f * gl_FragCoord.y) / resolution.y;
+    float y = (2.0f * gl_FragCoord.y) / resolution.y - 1.0f;
     float z = 1.0f;
     
     vec3 ray_nds = vec3(x, y, z);
@@ -19,5 +20,42 @@ void main() {
     vec3 ray_world = (p3d_ViewMatrixInverse * ray_eye).xyz;
     ray_world = normalize(ray_world);
 
-    p3d_FragColor = vec4(abs(ray_world.x), abs(ray_world.y), abs(ray_world.z), 1.0);
+    return ray_world;
+}
+
+float distance_from_sphere(in vec3 point, in vec3 center, float radius) {
+    return length(point - center) - radius;
+}
+
+vec3 ray_march(in vec3 ro, in vec3 rd) {
+    float total_distance_traveled = 0.0;
+    const int NUMBER_OF_STEPS = 32;
+    const float MINIMUM_HIT_DISTANCE = 0.001;
+    const float MAXIMUM_TRACE_DISTANCE = 1000.0;
+
+    for (int i = 0; i < NUMBER_OF_STEPS; ++i) {
+        vec3 current_position = ro + total_distance_traveled * rd;
+
+        float distance_to_closest = distance_from_sphere(current_position, vec3(0.0), 1.0);
+
+        if (distance_to_closest < MINIMUM_HIT_DISTANCE) {
+            return vec3(1.0, 0.0, 0.0);
+        }
+
+        if (total_distance_traveled > MAXIMUM_TRACE_DISTANCE) {
+            break;
+        }
+        total_distance_traveled += distance_to_closest;
+    }
+    
+    return vec3(0.0);
+}
+
+void main() {
+    vec3 ro = camera_position;
+    vec3 rd = gen_ray();
+
+    vec3 shaded_color = ray_march(ro, rd);
+
+    p3d_FragColor = vec4(shaded_color, 0.5);
 }
--- src/volume.py
+++ src/volume.py
@@ -1,6 +1,10 @@
 from direct.showbase.DirectObject import DirectObject
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import GraphicsWindow, Shader
+from direct.task import Task
+from panda3d.core import GraphicsWindow, Shader, TransparencyAttrib
+
+from lib.camera.camera_control import CameraControl
+from lib.util.util import defaultLight
 
 
 class App(DirectObject):
@@ -9,7 +13,15 @@ class App(DirectObject):
 
         self.base.setBackgroundColor(0, 0, 0, 1)
         # self.wireframe On()
-        self.base.cam.setPos(0, -40, 0)
+
+        defaultLight(self.base)
+        CameraControl(self.base)
+
+        cube = self.base.loader.loadModel("../assets/cube.glb")
+        if cube:
+            self.cube = cube
+
+        self.cube.reparentTo(self.base.render)
 
         shader = Shader.load(
             Shader.SL_GLSL,
@@ -24,7 +36,13 @@ class App(DirectObject):
         self.plane.reparentTo(self.base.render)
         self.plane.setP(90)
         self.plane.setScale(100)
+        self.plane.reparentTo(self.base.camera)
+        self.plane.setY(10)
         self.plane.setShader(shader)
+        self.plane.setTransparency(TransparencyAttrib.MAlpha)
+        self.plane.setBin("fixed", 0)
+        self.plane.setDepthTest(False)
+        self.plane.setDepthWrite(False)
 
         # For some reason this seems to be typed incorrectly; override the type
         window: GraphicsWindow = self.base.win  # type: ignore
@@ -34,6 +52,8 @@ class App(DirectObject):
 
         self.accept("window-event", self.handleWindowEvent)
 
+        base.taskMgr.add(self.updateCameraPosition, "update-camera-position")
+
     def handleWindowEvent(self, win: GraphicsWindow) -> None:
         newSize = (win.getXSize(), win.getYSize())
         if newSize[0] == self.windowSize[0] and newSize[1] == self.windowSize[1]:
@@ -41,6 +61,14 @@ class App(DirectObject):
 
         self.windowSize = newSize
         self.plane.setShaderInput("resolution", self.windowSize)
+
+    def updateCameraPosition(self, task: Task.Task) -> int:
+        self.plane.setShaderInput(
+            "camera_position",
+            self.base.camera.getPos(self.base.render),
+        )
+
+        return task.cont
 
 
 base = ShowBase()
