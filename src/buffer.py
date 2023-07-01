aa7cebf836edef4a6daf6fd979c0099ea566aa6d
Jacob Feddersen <jtfedd@gmail.com>
2023-07-01 11:12:10 -0500

    More playing around with buffers

--- src/buffer.py
+++ src/buffer.py
@@ -1,32 +1,55 @@
-import struct
-from random import random
+import random
 
+import numpy as np
 from direct.filter.FilterManager import FilterManager
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import GeomEnums, Shader, Texture
 
 from lib.util.optional import unwrap
+from lib.util.util import defaultLight
 
 base = ShowBase()
 
-SIZE = 3000000
+SIZE = 200000000
 
 print("Max buffer size: %d" % (base.win.get_gsg().get_max_buffer_texture_size()))
 
-buffer = Texture("texbuffer")
-buffer.setup_buffer_texture(SIZE, Texture.T_float, Texture.F_r32, GeomEnums.UH_dynamic)
+base.setFrameRateMeter(True)
+
+cube = unwrap(base.loader.loadModel("assets/cube.glb"))
+cube.reparentTo(base.render)
+defaultLight(base)
+
 
 manager = FilterManager(base.win, base.cam)  # type: ignore
-plane = unwrap(manager.renderSceneInto())
+scene = Texture("scene")
+plane = unwrap(manager.renderSceneInto(colortex=scene))
 
 plane.setShader(Shader.load(Shader.SL_GLSL, "shader/vertex.glsl", "shader/buffer.glsl"))
+plane.setShaderInput("scene", scene)
+
+size = SIZE + random.randrange(0, 10000)
+size2 = SIZE + random.randrange(0, 10000)
+rand = np.random.uniform(0.0, 1.0, size).astype(np.float32).tobytes()
+rand2 = np.random.uniform(0.0, 1.0, size2).astype(np.float32).tobytes()
+
+buffer = Texture("texbuffer")
+buffer.setup_buffer_texture(
+    max(size, size2), Texture.T_float, Texture.F_r32, GeomEnums.UH_dynamic
+)
 plane.setShaderInput("databuffer", buffer)
 
 
-def rebuild_data():
+def rebuild_data() -> None:
+    r_size = size
+    r_data = rand
+
+    if random.randrange(0, 2) == 1:
+        r_size = size2
+        r_data = rand2
+
     data = memoryview(buffer.modifyRamImage())
-    for i in range(SIZE):
-        data[i * 4 : i * 4 + 4] = struct.pack("f", random())
+    data[0 : r_size * 4] = r_data
 
 
 rebuild_data()
--- src/shader/buffer.glsl
+++ src/shader/buffer.glsl
@@ -2,9 +2,12 @@
 
 uniform samplerBuffer databuffer;
 
+uniform sampler2D scene;
+
 out vec4 p3d_FragColor;
 
 void main() {
+    vec4 scene_color = texelFetch(scene, ivec2(gl_FragCoord.xy), 0);
     float value = texelFetch(databuffer, int(gl_FragCoord.x + gl_FragCoord.y)).r;
-    p3d_FragColor = vec4(value, value, value, 1.0);
+    p3d_FragColor = scene_color * (1 - value) + vec4(value, value, value, value);
 }
