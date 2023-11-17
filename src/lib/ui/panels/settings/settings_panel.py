377db6f52f9c58b0d117f4325cf13963371c96c9
Jacob Feddersen <jtfedd@gmail.com>
2023-11-17 17:20:09 -0600

    I don't need this extra list

--- src/lib/ui/panels/settings/settings_panel.py
+++ src/lib/ui/panels/settings/settings_panel.py
@@ -1,17 +1,12 @@
-from typing import List
-
 from lib.ui.core.config import UIConfig
 from lib.ui.panels.components.panel_spacer import PanelSpacer
 from lib.ui.panels.components.panel_text_input import PanelTextInput
-from lib.ui.panels.core.panel_component import PanelComponent
 from lib.ui.panels.core.panel_content import PanelContent
 
 
 class SettingsPanel(PanelContent):
     def __init__(self, config: UIConfig) -> None:
         super().__init__(config)
-
-        self.components: List[PanelComponent] = []
 
         self.addComponent(PanelSpacer(self.root))
 
