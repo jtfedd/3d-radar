e59d1ef35808f07dbec27555986e8ff1daa15555
Jacob Feddersen <jtfedd@gmail.com>
2023-11-20 14:20:43 -0600

    Load and save config file

--- src/lib/app/app.py
+++ src/lib/app/app.py
@@ -1,15 +1,26 @@
+import atexit
+
 from direct.showbase.ShowBase import ShowBase
 
+from lib.app.app_config import AppConfig
 from lib.app.file_manager import FileManager
 from lib.ui.core.config import UIConfig
 from lib.ui.ui import UI
 
 
-class App(ShowBase):
-    def __init__(self) -> None:
-        super().__init__()
+class App:
+    def __init__(self, base: ShowBase) -> None:
+        self.base = base
 
         self.fileManager = FileManager()
 
-        self.uiConfig = UIConfig(self)
+        self.config = AppConfig()
+        self.config.fromJson(self.fileManager.loadConfig())
+
+        self.uiConfig = UIConfig(self.base)
         self.ui = UI(self.uiConfig)
+
+        atexit.register(self.saveConfig)
+
+    def saveConfig(self) -> None:
+        self.fileManager.saveConfig(self.config.toJson())
--- src/lib/app/file_manager.py
+++ src/lib/app/file_manager.py
@@ -9,3 +9,18 @@ class FileManager:
     def __init__(self) -> None:
         self.configPath = Path(user_config_dir(self.appName, False, ensure_exists=True))
         self.cachePath = Path(user_cache_dir(self.appName, False, ensure_exists=True))
+
+    def getConfigFile(self) -> Path:
+        return self.configPath.joinpath("config.json")
+
+    def loadConfig(self) -> str:
+        configPath = self.getConfigFile()
+        if not configPath.exists():
+            return "{}"
+
+        with self.configPath.joinpath("config.json").open("r", encoding="utf-8") as f:
+            return f.read()
+
+    def saveConfig(self, configStr: str) -> None:
+        with self.configPath.joinpath("config.json").open("w", encoding="utf-8") as f:
+            f.write(configStr)
