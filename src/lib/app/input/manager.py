d99b2e24099069ba9dcde75c248bb57c275f240e
Jacob Feddersen <jtfedd@gmail.com>
2023-11-25 14:22:04 -0600

    Tie the hide keybinding to the app config

--- src/lib/app/input/manager.py
+++ src/lib/app/input/manager.py
@@ -3,6 +3,7 @@ from direct.showbase.DirectObject import DirectObject
 from lib.app.focus.manager import FocusManager
 from lib.app.state import AppState
 from lib.util.events.event_dispatcher import EventDispatcher
+from lib.util.events.listener import Listener
 
 from .events import InputEvents
 
@@ -16,12 +17,19 @@ class InputManager(DirectObject):
     ):
         self.focusManager = focusManager
         self.events = events
+        self.state = state
 
         self.createBindings()
 
+        self.listener = Listener()
+        self.listener.listen(self.state.hideKeybinding, lambda _: self.createBindings())
+
     def createBindings(self) -> None:
         self.ignoreAll()
-        self.accept("h", lambda: self.send(self.events.onHide))
+
+        self.accept(
+            self.state.hideKeybinding.value, lambda: self.send(self.events.onHide)
+        )
 
         self.accept("wheel_up-up", lambda: self.events.scroll.send(-1))
         self.accept("wheel_down-up", lambda: self.events.scroll.send(1))
