bcba8a354126ef3a04a511b9fc3e72da1ca2bc1e
Jacob Feddersen <jtfedd@gmail.com>
2023-12-13 12:08:05 -0600

    Optimize sending data to the shader

--- src/lib/render_volume/render_volume.py
+++ src/lib/render_volume/render_volume.py
@@ -1,7 +1,6 @@
 import math
 from typing import Dict
 
-import numpy as np
 from direct.filter.FilterManager import FilterManager
 from direct.task import Task
 from panda3d.core import GeomEnums, GraphicsWindow, PTA_float, Shader, Texture
@@ -9,7 +8,9 @@ from panda3d.core import GeomEnums, GraphicsWindow, PTA_float, Shader, Texture
 from lib.app.context import AppContext
 from lib.app.events import AppEvents
 from lib.app.state import AppState
+from lib.model.data_type import DataType
 from lib.model.scan import Scan
+from lib.util import util
 from lib.util.events.listener import Listener
 from lib.util.optional import unwrap
 
@@ -27,6 +28,13 @@ class VolumeRenderer(Listener):
             Shader.SL_GLSL,
             vertex="shader/vertex.glsl",
             fragment="shader/fragment.glsl",
+        )
+
+        self.reflectivityScale = self.ctx.base.loader.loadTexture(
+            "assets/reflectivity_scale.png"
+        )
+        self.velocityScale = self.ctx.base.loader.loadTexture(
+            "assets/velocity_scale.png"
         )
 
         manager = FilterManager(self.ctx.base.win, self.ctx.base.cam)  # type: ignore
@@ -47,6 +55,10 @@ class VolumeRenderer(Listener):
             self.ctx.base.cam.node().getLens().getProjectionMatInv(),
         )
 
+        self.bufferSize = 0
+        self.buffer: Texture
+        self.setupBuffer()
+
         self.setDensityParams()
         self.listen(self.state.volumeMin, lambda _: self.setDensityParams())
         self.listen(self.state.volumeMax, lambda _: self.setDensityParams())
@@ -62,14 +74,33 @@ class VolumeRenderer(Listener):
         self.ctx.base.taskMgr.add(self.updateTime, "update-time")
 
         self.listen(state.animationFrame, lambda _: self.updateFrame())
+
+        self.updateDataType(state.dataType.value)
+        self.listen(state.dataType, self.updateDataType)
         self.listen(state.dataType, lambda _: self.updateFrame())
 
     def setData(self, data: Dict[str, Scan]) -> None:
         self.data = data
         self.updateFrame()
 
+    def updateDataType(self, dataType: DataType) -> None:
+        if dataType == DataType.REFLECTIVITY:
+            self.plane.setShaderInput("color_scale", self.reflectivityScale)
+
+            # Params to change to get velocity to be both negative and positive
+            # reflectivity = 0, 1
+            self.plane.setShaderInput("d_offset", 0)
+            self.plane.setShaderInput("d_factor", 1)
+        elif dataType == DataType.VELOCITY:
+            self.plane.setShaderInput("color_scale", self.velocityScale)
+
+            # Params to change to get velocity to be both negative and positive
+            # velocity = -0.5, 2
+            self.plane.setShaderInput("d_offset", -0.5)
+            self.plane.setShaderInput("d_factor", 2)
+
     def updateFrame(self) -> None:
-        if not self.state.animationFrame:
+        if not self.state.animationFrame.value:
             return
 
         if self.state.animationFrame.value not in self.data:
@@ -77,46 +108,34 @@ class VolumeRenderer(Listener):
 
         self.updateVolumeData(self.data[self.state.animationFrame.value])
 
-    def updateVolumeData(self, scan: Scan) -> None:
-        # velocity scale: -100 to 100
-        # reflectivity scale: -20 to 80
-        scaleMin = -20
-        scaleMax = 80
-
-        # Scale the data to 0..1, based on the scale range, and clip any outliers
-        data = scan.reflectivity
-        data = data - scaleMin
-        data = data / (scaleMax - scaleMin)
-        data = np.clip(data, 0, 1)
-
-        # Turn any nan into -1
-        data = np.nan_to_num(data, nan=-1)
-
-        # Get the bytes
-        dataBytes = data.flatten().tobytes()
-
-        # Params to change to get velocity to be both negative and positive
-        # reflectivity = 0, 1
-        # velocity = -0.5, 2
-        self.plane.setShaderInput("d_offset", 0)
-        self.plane.setShaderInput("d_factor", 1)
-
-        # Load the color scale image and send it to the shader
-        colorScale = self.ctx.base.loader.loadTexture("assets/reflectivity_scale.png")
-        self.plane.setShaderInput("color_scale", colorScale)
-
-        buffer = Texture("volume_data")
-        buffer.setup_buffer_texture(
-            len(dataBytes), Texture.T_float, Texture.F_r32, GeomEnums.UH_dynamic
+    def setupBuffer(self) -> None:
+        self.buffer = Texture("volume_data")
+        self.buffer.setupBufferTexture(
+            int(self.bufferSize / 4),  # buffer size is in bytes
+            Texture.T_float,
+            Texture.F_r32,
+            GeomEnums.UH_dynamic,
         )
-        self.plane.setShaderInput("volume_data", buffer)
+        self.plane.setShaderInput("volume_data", self.buffer)
 
-        dataView = memoryview(buffer.modifyRamImage())  # type: ignore
+    def updateVolumeData(self, scan: Scan) -> None:
+        dataBytes = scan.reflectivityBytes
+        if self.state.dataType.value == DataType.VELOCITY:
+            dataBytes = scan.velocityBytes
+
+        # Create a new buffer if necessary
+        if self.bufferSize < len(dataBytes):
+            self.bufferSize = util.nextPowerOf2(len(dataBytes))
+            self.setupBuffer()
+
+        # I would love to create a single memory view when the buffer is created
+        # and reuse that. It also seems to be faster. But for some reason the data
+        # is not sent properly that way.
+        dataView = memoryview(self.buffer.modifyRamImage())  # type: ignore
         dataView[0 : len(dataBytes)] = dataBytes
 
         self.plane.setShaderInput("el_min", float(scan.elevations[0]))
         self.plane.setShaderInput("el_max", float(scan.elevations[-1]))
-
         self.plane.setShaderInput("el_length", len(scan.elevations))
 
         elevations = PTA_float.emptyArray(20)
