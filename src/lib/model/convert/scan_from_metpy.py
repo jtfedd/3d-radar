ae092df716fe1d775cf118421a75dd6b9e4039b7
Jacob Feddersen <jtfedd@gmail.com>
2023-12-14 17:12:24 -0600

    remove some code that wasn't actually the issue

--- src/lib/model/convert/scan_from_metpy.py
+++ src/lib/model/convert/scan_from_metpy.py
@@ -124,9 +124,6 @@ def scanFromLevel2Data(record: Record, data: Level2File) -> Scan:
     spacing: float = 0
 
     for sweep in reflectivitySweeps:
-        if len(sweep.rays) == 0:
-            continue
-
         first = sweep.rays[0].first
         spacing = sweep.rays[0].spacing
         sweepReflectivity = np.stack(tuple(ray.data for ray in sweep.rays))
@@ -138,9 +135,6 @@ def scanFromLevel2Data(record: Record, data: Level2File) -> Scan:
         reflectivityLayers.append(sweepReflectivity)
 
     for sweep in velocitySweeps:
-        if len(sweep.rays) == 0:
-            continue
-
         first = sweep.rays[0].first
         spacing = sweep.rays[0].spacing
         sweepVelocity = np.stack(tuple(ray.data for ray in sweep.rays))
