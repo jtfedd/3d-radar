b477e4f3aa653f8cbc08d3ef3321fa5339eb7dbf
Jacob Feddersen <jtfedd@gmail.com>
2024-01-02 15:44:25 -0600

    Fix lints

--- src/test/model/test_location_query.py
+++ src/test/model/test_location_query.py
@@ -7,10 +7,10 @@ from lib.model.location_query import LocationQuery, compareLocationQueries
 
 class TestLocationQuery(unittest.TestCase):
     def testComparator(self) -> None:
-        a = LocationQuery("asdf", 2, [])
-        b = LocationQuery("asdf", 2, [Location("Hi", "There", 0, 0)])
-        c = LocationQuery("asdf", 3, [])
-        d = LocationQuery("fdsa", 1, [])
+        a = LocationQuery("asdf", 2, [], 0)
+        b = LocationQuery("asdf", 2, [Location("Hi", "There", 0, 0)], 1)
+        c = LocationQuery("asdf", 3, [], 2)
+        d = LocationQuery("fdsa", 1, [], 3)
 
         self.assertEqual(compareLocationQueries(a, a), 0)
         self.assertEqual(compareLocationQueries(a, b), 0)
@@ -20,10 +20,10 @@ class TestLocationQuery(unittest.TestCase):
         self.assertEqual(compareLocationQueries(d, a), 1)
 
     def testSort(self) -> None:
-        a = LocationQuery("asdf", 2, [])
-        b = LocationQuery("qwert", 2, [Location("Hi", "There", 0, 0)])
-        c = LocationQuery("asdf", 3, [])
-        d = LocationQuery("fdsa", 1, [])
+        a = LocationQuery("asdf", 2, [], 2)
+        b = LocationQuery("qwert", 2, [Location("Hi", "There", 0, 0)], 5)
+        c = LocationQuery("asdf", 3, [], 8)
+        d = LocationQuery("fdsa", 1, [], 11)
 
         self.assertListEqual(
             sorted([a, b, c, d], key=cmp_to_key(compareLocationQueries)),
