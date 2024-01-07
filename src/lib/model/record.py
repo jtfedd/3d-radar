ccf838d3ba96c1d0b887bc8ffb8378aedaaf514f
Jacob Feddersen <jtfedd@gmail.com>
2024-01-06 21:11:15 -0600

    Remove prefix from aws key

--- src/lib/model/record.py
+++ src/lib/model/record.py
@@ -3,7 +3,6 @@ from typing import Self
 
 
 class Record:
-    PREFIX_FMT = "{1:02}/{2:02}/{3:02}/{0}/"
     FILE_FMT = "{0}{1:02}{2:02}{3:02}_{4:02}{5:02}{6:02}"
 
     def __init__(self, station: str, time: datetime.datetime, extension: str = "V06"):
@@ -12,10 +11,7 @@ class Record:
         self.extension = extension
 
     def awsKey(self) -> str:
-        return self.format(self.awsPrefix() + self.key() + "_" + self.extension)
-
-    def awsPrefix(self) -> str:
-        return self.format(self.PREFIX_FMT)
+        return self.format(self.key() + "_" + self.extension)
 
     def key(self) -> str:
         return self.format(self.FILE_FMT)
@@ -33,20 +29,7 @@ class Record:
 
     @classmethod
     def parse(cls, awsKey: str) -> Self | None:
-        pathParts = awsKey.split("/")
-        if len(pathParts) != 5:
-            return None
-
-        station = pathParts[3]
-
-        try:
-            year = int(pathParts[0])
-            month = int(pathParts[1])
-            day = int(pathParts[2])
-        except ValueError:
-            return None
-
-        fileParts = pathParts[4].split("_")
+        fileParts = awsKey.split("_")
         if len(fileParts) != 3:
             return None
 
@@ -54,9 +37,18 @@ class Record:
         time = fileParts[1]
         extension = fileParts[2]
 
-        check = f"{station}{year:02}{month:02}{day:02}"
+        if len(stationDate) != 12:
+            return None
+        if len(time) != 6:
+            return None
 
-        if stationDate != check:
+        station = stationDate[0:4]
+
+        try:
+            year = int(stationDate[4:8])
+            month = int(stationDate[8:10])
+            day = int(stationDate[10:12])
+        except ValueError:
             return None
 
         try:
