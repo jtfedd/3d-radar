e9babb7fa9f9f504d39af45ddb4f7dfa1cc0c547
Jacob Feddersen <jtfedd@gmail.com>
2024-01-10 15:34:08 -0600

    Set gl profile version on startup

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import loadPrcFileData
 
 from lib.app.app import App
 
 if __name__ == "__main__":
+    loadPrcFileData("", "gl-version 3 2")
     base = ShowBase()
     app = App(base)
     base.run()
