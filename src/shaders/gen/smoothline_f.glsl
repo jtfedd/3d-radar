afdd2c4b22435d6fe94c44d0d1bea19d5280f7a1
jtfedd <jtfedd@gmail.com>
2024-05-18 21:45:28 -0500

    Delete alternate shader

--- src/shaders/gen/smoothline_f.glsl
+++
@@ -1,8 +0,0 @@
-#version 330
-
-uniform vec4 p3d_ColorScale;
-out vec4 p3d_FragColor;
-
-void main(void) {
-	p3d_FragColor = p3d_ColorScale;
-}
--- src/shaders/smoothline_v.glsl
+++
@@ -1,8 +0,0 @@
-#version 330
-
-uniform mat4 p3d_ModelViewProjectionMatrix;
-in vec4 p3d_Vertex;
-
-void main(void) {
-	gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
-}
