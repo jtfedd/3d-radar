f097fb9109a34bdc5d0438e275ed48e41dcda2b0
jtfedd <jtfedd@gmail.com>
2024-08-18 15:52:02 -0500

    Read and write from cache

--- src/lib/app/files/manager.py
+++ src/lib/app/files/manager.py
@@ -26,3 +26,19 @@ class FileManager:
 
         print("Removing", str(file))
         file.unlink()
+
+    def getCacheFileBytes(self, filename: str) -> bytes | None:
+        filepath = self.getCacheFile(filename)
+        if not filepath.exists():
+            return None
+
+        print("Reading from cache", filename)
+
+        with open(filepath, "rb") as file:
+            return file.read()
+
+    def saveCacheFileBytes(self, filename: str, data: bytes) -> None:
+        print("Writing to cache", filename)
+
+        with open(self.getCacheFile(filename), "wb") as file:
+            file.write(data)
