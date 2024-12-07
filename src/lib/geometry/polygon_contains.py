df1ddd89e7b1c0db0ea276b6cc5ec5d8e3433418
jtfedd <jtfedd@gmail.com>
2024-12-06 21:46:12 -0600

    Implement polygonContains method

--- src/lib/geometry/polygon_contains.py
+++ src/lib/geometry/polygon_contains.py
@@ -4,5 +4,27 @@ from lib.model.geo_point import GeoPoint
 
 
 def polygonContains(ring: List[GeoPoint], p: GeoPoint) -> bool:
-    # TODO implement
-    return False
+    contains = False
+
+    i = 0
+    j = len(ring) - 1
+
+    testX = p.lon
+    testY = p.lat
+
+    while i < len(ring):
+        vertIX = ring[i].lon
+        vertIY = ring[i].lat
+
+        vertJX = ring[j].lon
+        vertJY = ring[j].lat
+
+        if ((vertIY > testY) != (vertJY > testY)) and (
+            testX < (vertJX - vertIX) * (testY - vertIY) / (vertJY - vertIY) + vertIX
+        ):
+            contains = not contains
+
+        j = i
+        i += 1
+
+    return contains
