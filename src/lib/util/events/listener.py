ae9c033c4372fd878e2f5575cbe58ad9d857256d
jtfedd <jtfedd@gmail.com>
2025-02-06 07:00:19 -0600

    Expand utils

--- src/lib/util/events/listener.py
+++ src/lib/util/events/listener.py
@@ -30,16 +30,20 @@ class Listener:
         self,
         dispatcher: EventDispatcher[T],
         callback: Callable[[], None],
+        triggerImmediately: bool = False,
     ) -> None:
+        if triggerImmediately:
+            callback()
         self.subscriptions.append(dispatcher.listen(lambda _: callback()))
 
     def triggerMany(  # type: ignore
         self,
         dispatchers: List[EventDispatcher[Any]],
         callback: Callable[[], None],
+        triggerImmediately: bool = False,
     ) -> None:
         for dispatcher in dispatchers:
-            self.trigger(dispatcher, callback)
+            self.trigger(dispatcher, callback, triggerImmediately=triggerImmediately)
 
     def destroy(self) -> None:
         for sub in self.subscriptions:
