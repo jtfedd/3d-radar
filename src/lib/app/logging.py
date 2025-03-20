283ad009194593298aef417aeb9ca143074a9672
jtfedd <jtfedd@gmail.com>
2025-03-19 20:25:04 -0500

    Start trying out granular logging

--- src/lib/app/logging.py
+++ src/lib/app/logging.py
@@ -1,6 +1,27 @@
+from enum import Enum
+from typing import Dict
+
 from direct.directnotify.DirectNotify import DirectNotify
 from direct.directnotify.Notifier import Notifier
+from panda3d.core import loadPrcFileData
 
 
-def newLogger(name: str) -> Notifier:
-    return DirectNotify().newCategory(name)
+class LoggingLevel(Enum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+
+class LoggingManager:
+    def __init__(self) -> None:
+        self.loggers: Dict[str, Notifier] = {}
+
+    def newLogger(self, name: str) -> Notifier:
+        logger = DirectNotify().newCategory(name)
+        self.loggers[name] = logger
+        return logger
+
+    def setLevel(self, name: str, level: LoggingLevel) -> None:
+        loadPrcFileData("", f"notify-category-{name} {level}")
